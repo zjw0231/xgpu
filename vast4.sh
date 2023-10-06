539b4d78b83b96b4fa2673fd610c688ee18d2395
zjw0231 <114279968+zjw0231@users.noreply.github.com>
2023-10-07 01:52:36 +0800

    Update vast4.sh

--- vast4.sh
+++ vast4.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x2CCed186C0B172443884B0dAe52695e847EE010d/account = 0x2CCed186C0B172443884B0dAe52695e847EE010d/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x2CCed186C0B172443884B0dAe52695e847EE010d/g' config.conf
 sleep 5
 echo "---------------------------"
 
